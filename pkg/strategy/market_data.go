5d2d906faa5df7a5120b9293ded389a1d6857531
bmf-san <bmf.infomation@gmail.com>
2026-03-16 16:28:47 +0900

    style: gofmt pkg/strategy/market_data.go

--- pkg/strategy/market_data.go
+++ pkg/strategy/market_data.go
@@ -6,16 +6,16 @@ import "time"
 // This is a standalone definition so external consumers do not need to import
 // the internal domain package.
 type MarketData struct {
-Symbol      string
-ProductCode string
-Timestamp   time.Time
-Price       float64
-Volume      float64
-BestBid     float64
-BestAsk     float64
-Spread      float64
-Open        float64
-High        float64
-Low         float64
-Close       float64
+	Symbol      string
+	ProductCode string
+	Timestamp   time.Time
+	Price       float64
+	Volume      float64
+	BestBid     float64
+	BestAsk     float64
+	Spread      float64
+	Open        float64
+	High        float64
+	Low         float64
+	Close       float64
 }
